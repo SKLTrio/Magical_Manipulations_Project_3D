f77d0179010fbb35c1e67f64ee0c924d09591ce0
Neve Lane <123910307+SKLTrio@users.noreply.github.com>
2023-12-30 00:43:11 +0000

    Ability Switcher Done!

--- Assets/Magical_Weapons_System/Scripts/Explosion_Ability_Script.cs
+++ Assets/Magical_Weapons_System/Scripts/Explosion_Ability_Script.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class Explosion_Ability_Script : MonoBehaviour
 {
-    //[SerializeField]
-    //public ParticleSystem 
+    [SerializeField]
+    public ParticleSystem Explosion_PS;
+
+    [SerializeField]
+    public float Explosion_Cooldown_Period;
+
+    public void On_Use(InputAction.CallbackContext Context)
+    {
+        Start_Explosion();
+    }
+
+    public void Start_Explosion()
+    {
+        Debug.Log("BOOM");
+    }
 }
---
+++ Assets/Magical_Weapons_System/Scripts/Magical_Ability_Switch_Interface.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Magical_Ability_Switch_Interface : MonoBehaviour
+{
+    [SerializeField]
+    public int Selected_Ability = 0;
+
+    [SerializeField]
+    public GameObject[] Ability_Image;
+
+    [SerializeField]
+    public GameObject[] Ability_Ammo_Image;
+
+    public void Start()
+    {
+        Select_Ability();
+    }
+
+    public void Update()
+    {
+        int Previously_Selected_Ability = Selected_Ability;
+
+        float Scroll_Wheel_Value = Input.mouseScrollDelta.y;
+
+        if (Scroll_Wheel_Value < 0f)
+        {
+            if (Selected_Ability >= transform.childCount - 1)
+            {
+                Selected_Ability = 0;
+            }
+
+            else
+            {
+                Selected_Ability++;
+            }
+        }
+
+        if (Scroll_Wheel_Value > 0f)
+        {
+            if (Selected_Ability <= 0)
+            {
+                Selected_Ability = transform.childCount - 1;
+            }
+
+            else
+            {
+                Selected_Ability--;
+            }
+        }
+
+        if (Previously_Selected_Ability != Selected_Ability)
+        {
+            Select_Ability();
+        }
+    }
+
+    public void Select_Ability()
+    {
+        int i = 0;
+
+        foreach (Transform Ability in transform)
+        {
+            if (i == Selected_Ability)
+            {
+                Ability.gameObject.SetActive(true);
+                Ability_Image[i].SetActive(true);
+                Ability_Ammo_Image[i].SetActive(true);
+            }
+
+            else
+            {
+                Ability.gameObject.SetActive(false);
+                Ability_Image[i].SetActive(false);
+                Ability_Ammo_Image[i].SetActive(false);
+            }
+
+            i++;
+        }
+    }
+}
